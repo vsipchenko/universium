a3f8105a6ce5c05587813cdd231d6420418d1960
vitaliisipchenko <vitaliisipchenko@gmail.com>
2022-12-16 12:32:51 +0200

    add models and raw data

--- actors/models.py
+++ actors/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class Actor(models.Model):
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+
+    first_name = models.CharField(max_length=256)
+    last_name = models.CharField(max_length=256)
+    gender = models.CharField(max_length=256, choices=GENDER_CHOICES)
+
+    class Meta:
+        db_table = 'actors'
+
+
+class Role(models.Model):
+    actor_id = models.ForeignKey('Actor', on_delete=models.DO_NOTHING)
+    movie_id = models.ForeignKey('movies.Movie', on_delete=models.DO_NOTHING)
+    role = models.CharField(max_length=256)
+
+    class Meta:
+        db_table = 'roles'
+
+
+class Director(models.Model):
+    first_name = models.CharField(max_length=256)
+    last_name = models.CharField(max_length=256)
+
+    class Meta:
+        db_table = 'directors'
+
+
+class DirectorGenre(models.Model):
+    director_id = models.ForeignKey('Director', on_delete=models.DO_NOTHING)
+    genre = models.CharField(max_length=256)
+    prob = models.FloatField()
+
+    class Meta:
+        db_table = 'directors_genres'
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    name = models.CharField(max_length=256)
+    year = models.IntegerField()
+    rank = models.FloatField()
+
+    class Meta:
+        db_table = 'movies'
+
+
+class MovieGenre(models.Model):
+    movie_id = models.ForeignKey('Movie', on_delete=models.DO_NOTHING)
+    genre = models.CharField(max_length=256)
+
+    class Meta:
+        db_table = 'movies_genres'
+
+
+class MovieDirector(models.Model):
+    director_id = ''
+    movie_id = models.ForeignKey('Movie', on_delete=models.DO_NOTHING)
+
+    class Meta:
+        db_table = 'movies_directors'
